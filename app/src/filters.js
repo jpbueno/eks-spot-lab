ec240d51b157554db39bad5ded5ac49fc54d085a
JP Santana <jp.bueno@gmail.com>
2021-05-01 20:50:17 -0400

    changed instance type to t4g.medium in README

--- app/src/filters.js
+++
@@ -1,8 +0,0 @@
-const PIXI = require('pixi.js')
-const BRIGHTNESS_FILTER = new PIXI.filters.ColorMatrixFilter()
-BRIGHTNESS_FILTER.brightness(1.3)
-
-const DESATURATION_FILTER = new PIXI.filters.ColorMatrixFilter()
-DESATURATION_FILTER.desaturate()
-
-export { BRIGHTNESS_FILTER, DESATURATION_FILTER }
